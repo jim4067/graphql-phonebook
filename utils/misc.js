41193099afa2e2d97c0a42756dbae033cfb84304
jim4067 <jimmyimpulse2@gmail.com>
2020-11-18 19:59:30 +0300

    feat: misc data and modules

---
+++ utils/misc.js
@@ -0,0 +1,23 @@
+//the data when you are not connected to a database
+let persons = [
+    {
+        name: "Homer Simpson",
+        phone: "040-123543",
+        street: "Tapiolankatu 5 A",
+        city: "Espoo",
+        id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+    },
+    {
+        name: "Marge Simpson",
+        phone: "040-432342",
+        street: "Malminkaari 10 A",
+        city: "Helsinki",
+        id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+    },
+    {
+        name: "Bart Simpson",
+        street: "Nallemäentie 22 C",
+        city: "Helsinki",
+        id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+    },
+];
